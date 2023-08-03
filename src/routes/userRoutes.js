fab70d767fecbe22ae06500fcaae1458848525c7
Douglas Martins <douglas.martins@auditornfe.com.br>
2023-08-03 14:35:05 -0300

    feat: Criando middleware para verificiar o tipo do usuário

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const user = express.Router();
 const userCtrl = require('../controllers/userCtrl');
+const { checkUserType } = require('../middleware/user_type');
+require('../middleware/user_type');
 
 user.post('/novo-usuario', userCtrl.createUser);
 user.put('/atualiza-usuario', userCtrl.updateUser);
@@ -14,6 +16,6 @@ user.post('/solicita-nova-senha', userCtrl.newPassword)
 user.post('/compare-codigo-senha', userCtrl.comparePasswordCode);
 user.post('/nova-senha', userCtrl.updatePassword);
 
-user.put('/desabilita-usuario/', userCtrl.disableUser);
+user.put('/desabilita-usuario/', checkUserType, userCtrl.disableUser);
 
 module.exports = user;
